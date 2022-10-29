d8cd1e862e03f6794661a8a1ad3ec05d130abc0f
fluffysnowman <thefluffysnowmanyt@gmail.com>
2022-10-29 14:59:05 +0530

    added fish folder

---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,21 @@
+# This function is run every time fish displays a new prompt.
+function fish_prompt
+
+  # Display present working directory.
+  set_color --bold brblue # pwd color
+  set pwdLen (string length $PWD)
+
+  # Always display "fish" prompt on new line.
+  set_color normal
+  # This uses printf instead of echo to output a leading newline.
+  printf '\\n>  ' # uses Unicode for tropical fish emoji &#x1F420;
+
+
+end
+
+alias gpl="git pull"
+alias ga="git add ."
+alias gc="git commit -m"
+alias gp="git push"
+alias cls="clear"
+alias key="cat /home/fluffy/Desktop/k/key"
